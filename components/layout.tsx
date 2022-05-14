50fd64c933126cda7b09a4ec2abca45de4909cac
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2022-05-14 15:20:28 -0600

    LayoutProps made

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import variables from '../styles/variables.module.scss'
 import styles from '../styles/Home.module.scss'
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+interface LayoutProps {
+    children: React.ReactNode;
+}
+
+
+export default function Layout({ children }: LayoutProps) {
     return <div
         className={styles.container}
         color={variables.primaryColor}>{children}
