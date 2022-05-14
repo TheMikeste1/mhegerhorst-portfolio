ca3461f32484637ef5a82178a7a80a44c8b3c6a6
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2022-05-14 16:20:12 -0600

    Added Jest test framework and some examples
    Src: https://nextjs.org/docs/testing#setting-up-jest-with-the-rust-compiler

---
+++ __test__/snapshot.test.tsx
@@ -0,0 +1,11 @@
+// __tests__/snapshot.test.tsx
+
+import { render } from '@testing-library/react'
+import Home from '../pages/index'
+import React from "react";
+
+it('renders homepage unchanged', () => {
+    const { container } = render(<Home />);
+
+    expect(container).toMatchSnapshot()
+},);
